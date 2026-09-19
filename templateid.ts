456b8a52414be03245ffb09c293c8633c37a2453
Saulo Victor <saulovictor@hotmail.com.br>
2026-09-18 23:48:06 -0300

    Update templateid.ts

--- templateid.ts
+++ templateid.ts
@@ -8,5 +8,5 @@
 //   3. Troque abaixo o trecho que vem depois de "#pub:"
 //   4. Commit. Os notebooks pegam sozinhos na proxima abertura.
 //
-// templateid: _Cc7gYahUf0ok
+// templateid: _DAcWsxDFcDbw
 // ===========================================================================
